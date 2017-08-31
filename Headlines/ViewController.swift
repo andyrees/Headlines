0f6e2277b5112dbb4525cad1082d405e99674467
Andy Rees <andrewrees@andys-imac.home>
2017-08-31 13:30:06 +0100

    Initial Commit

---
+++ Headlines/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Headlines
+//
+//  Created by Andy Rees on 31/08/2017.
+//  Copyright © 2017 rantcode.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
